86da0a074599c008f8a7c9a94150fbfc037d754d
ayushkothari1 <pahadiayush61@gmail.com>
2024-09-13 13:14:19 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "ayush@google.com"
+var accountPassword = "12345"
+accountCity = "Deharadun"
+let accountState;
+
+
+// accountId = 2
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bangluru"
+
+console.log(accountId);
+
+// /*
+// prefer not to use var
+// bacause of issue in block scope and functional scope
+// /*
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use script";//treat all js code as newer version
+
+// alert( 3+ 3)
+
+console.log(3 
+    + 3); //code readability should be high
+
+
+
+console.log("ayush");
+
+
+let name = "ayush";
+let age = 18
+let isLogggedIn = false
+let state;
+
+// number=> 2 to power of 53
+//bigint
+//string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
